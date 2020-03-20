57fb063af95ac5210d41ee4f6df8af7eef94127d
Jeremy Ward <jeremy@jmichaelward.com>
2020-03-20 09:58:50 -0500

    Initial commit.

---
+++ public/wp-config.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+/**
+ * Let's just go ahead and keep most of this outside of our public directory, shall we?
+ */
+$config = dirname( __FILE__, 2 ) . '/config/config.php';
+
+if ( ! is_readable( $config ) ) {
+    echo '<h1>Nothing to see here, folks.';
+    return;
+}
+
+require_once $config;
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once( ABSPATH . 'wp-settings.php' );

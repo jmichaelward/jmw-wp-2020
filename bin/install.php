b0bcadae60f54cccb00983d3e42380b4ac42e969
Jeremy Ward <jeremy@jmichaelward.com>
2020-04-02 14:19:26 -0500

    Add documentation to the install script.

--- bin/install.php
+++ bin/install.php
@@ -1,5 +1,18 @@
 #!/usr/bin/env php
 <?php
+/**
+ * Helper installer script for setting up the local dev environment.
+ *
+ * Note: This is not yet elegant enough to check for missing dependencies. For
+ * instance, if Yarn is not installed on the local machine, this script will
+ * fail to complete. That said, because it is run on the Composer post-install
+ * event, all of the dependencies you'll need to set up the site should be present,
+ * and the script can be manually re-triggered once the remaining build tools are in place.
+ *
+ * This script will get updated as the development processes are changed. For instance,
+ * it may not be necessary in the future to run the theme build steps if the themes themselves
+ * ship with built assets in tagged releases.
+ */
 
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\Process\Process;
@@ -12,11 +25,18 @@ $root = dirname( __FILE__, 2 );
 
 require_once "{$root}/public/vendor/autoload.php";
 
+/*
+ * Copy the example environment file into config if it's not set up to get
+ * some initial values.
+ */
 if ( ! is_readable( "{$root}/config/.env" ) ) {
 	$process = new Process( [ 'cp', '.env.example', '.env' ], "{$root}/config" );
 	$process->run();
 }
 
+/*
+ * Attempt to load the .env configuration. Bail if it can't be found.
+ */
 try {
 	$dotenv = new Dotenv();
 	$dotenv->load( "{$root}/config/.env" );
@@ -24,9 +44,18 @@ try {
 	echo "No .env file found in configuration. Could not complete installation.\n";
 	exit( 1 );
 }
+
+// Get theme values.
 $theme     = $_ENV['CURRENT_THEME'];
 $theme_dir = "{$root}/public/wp-content/themes/{$theme}";
 
+/*
+ * Set up individual processes to execute.
+ *
+ * Each step is given a name so we can make particular checks within the foreach loop.
+ * If there was ever a need to abstract this out, we could, but for now, it's fine just being
+ * a simple array of parameters to pass into new Process objects.
+ */
 $processes = [
 	'install_yarn'   => [
 		[ 'yarn', 'install ' ],
@@ -42,6 +71,7 @@ $processes = [
 	],
 ];
 
+// Loop through the parameters and run the individual processes with output.
 foreach ( $processes as $name => $params ) {
 	if ( 'install_yarn' === $name && is_dir( $theme_dir . '/node_modules' ) ) {
 		continue;
